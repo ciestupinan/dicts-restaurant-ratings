5a4692917a6bbe589a908ba37f618c75b24677fa
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-08 15:18:39 -0700

    Added option for user to input restaurant name and rating.

--- ratings.py
+++ ratings.py
@@ -13,5 +13,19 @@ with open(filename) as file:
 		restaurant_name, restaurant_rating = line.split(":")
 		restaurant_ratings[restaurant_name] = restaurant_rating.rstrip("\n")
 
+	add_restaurant = input("Would you like to enter a restaurant? [yes/no] ")
+	if add_restaurant.lower() == "yes":
+		prompt_restaurant_name = input("Enter a restaurant name: ")
+		while prompt_restaurant_name == "":
+			print("Please enter a valid restaurant name.")
+			prompt_restaurant_name = input("Enter a restaurant name: ")
+
+		prompt_restaurant_rating = input("Rate the restaurant from 1 to 5: ")
+		while not prompt_restaurant_rating.isdigit() and int(prompt_restaurant_rating) > 5:
+			print("Please enter a valid restaurant rating.")
+			prompt_restaurant_rating = input("Rate the restaurant from 1 to 5: ")
+
+		restaurant_ratings[prompt_restaurant_name] = prompt_restaurant_rating
+
 	for k, v in sorted(restaurant_ratings.items()):
 		print("{} is rated at {}.".format(k, v))
